29698b7df162df2f11346f984cbc9c869397ef13
Fabricio Milanio <milanio.fabricio@gmail.com>
2025-11-26 10:11:47 -0300

    ajuste de desempenho da aplicação

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <--- Importante: Adicionado aqui
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -19,6 +20,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        // Força HTTPS se estiver em ambiente de produção
+        if($this->app->environment('production')) {
+            URL::forceScheme('https');
+        }
     }
 }
